fb1d30bed0bb5226d8f28fd13c14f775682b53d2
savinda <savindamaddd@gmail.com>
2016-08-24 11:48:43 +0530

    new login test 1

--- app/src/main/java/com/group12/smartrms/smartrms/LoginActivity.java
+++ app/src/main/java/com/group12/smartrms/smartrms/LoginActivity.java
@@ -10,36 +10,81 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-public class LoginActivity extends Activity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
 
     EditText etUsername, etPassword;
-    Button bLogin;
-
+    private static final String URL = "http://smartrmswebb.azurewebsites.net/loginNew.php";
     String username, password;
+    StringRequest request;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        etUsername = (EditText) findViewById(R.id.etUsername);
-        etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
 
-        bLogin = (Button) findViewById(R.id.bLogin);
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        bLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if(jsonObject.names().get(0).equals("success")){
+                                String userID = jsonObject.getString("success");
+                                Intent intent = new Intent(LoginActivity.this,UserAreaActivity.class);
+                                intent.putExtra("userID",userID);
+                                startActivity(intent);
+                            }
+                            else{
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        HashMap<String,String> hashMap = new HashMap<String, String>();
+                        hashMap.put("username",etUsername.getText().toString()); //todo encript username and password
+                        hashMap.put("password",etPassword.getText().toString());
+                        return hashMap;
+                    }
+                };
+            }
+        });
     }
 
-    public void Login(View view) {
-        username = etUsername.getText().toString();
-        password = etPassword.getText().toString();
-        Intent intent = new Intent(LoginActivity.this, TableTypeActivity.class);
-
-        LoginRequest loginRequest = new LoginRequest(this);
-        loginRequest.setIntent(intent);
-        loginRequest.execute(username, password);
-        String userID = loginRequest.user_ID;
-
-    }
 }
---
+++ app/src/main/java/com/group12/smartrms/smartrms/UserAreaActivity.java
@@ -0,0 +1,13 @@
+package com.group12.smartrms.smartrms;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserAreaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_area);
+    }
+}
